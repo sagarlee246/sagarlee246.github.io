d4165b708cb331025acf29e0e908ecac7f28e91e
sagarlee246 <134967516+sagarlee246@users.noreply.github.com>
2026-04-08 14:49:42 +0000

    Deploying to gh-pages from @ sagarlee246/sagarlee246.github.io@b53133a4ec0697accc792913b03d006266b8f3a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of projects in physics and data science.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Projects/";
